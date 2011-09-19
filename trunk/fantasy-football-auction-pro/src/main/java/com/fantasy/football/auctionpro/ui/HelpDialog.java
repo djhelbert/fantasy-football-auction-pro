aa25e7008557ae30a35f97099eb1bf8ae219d42e
djhelbert@gmail.com <djhelbert@gmail.com@d9119fe0-fd3f-73ad-78f7-a566a1425193>
2011-09-19 21:08:36 +0000

    New

---
+++ trunk/fantasy-football-auction-pro/src/main/java/com/fantasy/football/auctionpro/ui/HelpDialog.java
@@ -0,0 +1,135 @@
+package com.fantasy.football.auctionpro.ui;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Map;
+
+import javax.swing.JDialog;
+import javax.swing.JEditorPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTree;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+import com.fantasy.football.auctionpro.MainApp;
+
+/**
+ * Help Frame
+ * 
+ * @author dhelbert
+ *
+ */
+public class HelpDialog extends JDialog implements TreeSelectionListener, HyperlinkListener {
+
+	/** Serial Version UID */
+	private static final long serialVersionUID = 7217796584898452233L;
+
+	/** Tree */
+	private JTree tree;
+	
+	/** Editor Pane */
+	private JEditorPane pane = new JEditorPane();
+	
+	/** Entries */
+	private Map<String,URL> entries;
+	
+	/** Text Field */
+	private JTextField textField = new JTextField(" ");
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param entries
+	 */
+	public HelpDialog(Map<String,URL> entries) {
+		super(MainApp.getMainFrame(),"Help",true);
+		
+		this.entries = entries;
+		
+		init();
+		
+		setSize(700, 500);
+		
+		Util.centerComponent(this);
+	}
+	
+	/**
+	 * Init
+	 */
+	private void init() {
+		Container content = getContentPane();
+		
+		content.setLayout(new BorderLayout(2,2));
+		
+		DefaultMutableTreeNode root = new DefaultMutableTreeNode("Topics");
+		
+		tree = new JTree(root);
+		
+		DefaultMutableTreeNode child;
+	    
+	    for(String key : entries.keySet()) {
+	    	child = new DefaultMutableTreeNode(key);
+	    	root.add(child);
+	    }
+	    
+	    tree.expandRow(0);
+	    tree.addTreeSelectionListener(this);
+	    content.add(new JScrollPane(tree), BorderLayout.WEST);
+	    
+	    pane.setEditable(false);
+	    pane.addHyperlinkListener(this);
+	    
+	    content.add(new JScrollPane(pane),BorderLayout.CENTER);
+	    
+	    textField.setEditable(false);
+	    
+	    content.add(textField,BorderLayout.SOUTH);
+	}
+
+	/**
+	 * Value Changed
+	 * 
+	 * @param e TreeSelectionEvent
+	 */
+	public void valueChanged(TreeSelectionEvent e) {
+		String key = tree.getLastSelectedPathComponent().toString();
+		
+		try {
+			if( entries.get(key) != null ) {
+				pane.setPage(entries.get(key));
+			}
+		} catch (IOException err) {
+			err.printStackTrace();
+		}
+	}
+
+	/**
+	 * Hyper Link Update
+	 * 
+	 * @param e HyperlinkEvent
+	 */
+	public void hyperlinkUpdate(HyperlinkEvent e) {
+		if( e.getEventType() == HyperlinkEvent.EventType.EXITED ) {
+			textField.setText(" ");
+		}
+		else if( e.getEventType() == HyperlinkEvent.EventType.ENTERED ) {
+			textField.setText(e.getURL().toString());
+		}
+		else if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+	        try {
+	          pane.setPage(e.getURL());
+	          textField.setText(e.getURL().toExternalForm());
+	        } 
+	        catch(IOException err) {
+	          err.printStackTrace();
+	        }
+	      }
+	}
+
+}
